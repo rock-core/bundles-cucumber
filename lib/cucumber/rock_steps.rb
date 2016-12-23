af6749fad67e3089701e8d440e157e1f6d2a47af
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-12-23 13:48:57 -0200

    anchor all patterns
    I believed cucumber was doing it already, but it does not seem so

--- lib/cucumber/rock_steps.rb
+++ lib/cucumber/rock_steps.rb
@@ -1,6 +1,6 @@
 require 'cucumber/rock_helpers'
 
-Given(/the (\w+) robot starting at (.*) in (?:the )?(.*)/) do |robot_name, start_position, world|
+Given(/^the (\w+) robot starting at (.*) in (?:the )?(.*)$/) do |robot_name, start_position, world|
     if start_position == 'origin'
         start_position = Hash[x: 0, y: 0, z: 0, yaw: 0]
     else
@@ -19,33 +19,34 @@ Given(/the (\w+) robot starting at (.*) in (?:the )?(.*)/) do |robot_name, start
         pose: pose
 
 end
-When(/it runs the (.*) (action|definition) with (.*)/) do |action_name, action_kind, raw_arguments|
+When(/^it runs the (.*) (action|definition) with (.*)$/) do |action_name, action_kind, raw_arguments|
     action_name = Cucumber::RockHelpers.massage_action_name(action_name, action_kind)
     arguments   = Roby::App::CucumberHelpers.parse_arguments(raw_arguments)
     roby_controller.start_job "When it runs #{action_name} with #{raw_arguments}",
         action_name, **arguments
 end
-Then(/the pose reaches (.*) with a tolerance of (.*) within (.*)/) do |pose, tolerance, timeout|
+Then(/^the pose reaches (.*) with a tolerance of (.*) within (.*)$/) do |pose, tolerance, timeout|
     pose, position_tolerance, orientation_tolerance = Cucumber::RockHelpers.parse_pose_and_tolerance(pose, tolerance)
     timeout, _ = Roby::App::CucumberHelpers.parse_numerical_value(timeout)
     roby_controller.run_job 'cucumber_reach_pose',
         pose: pose, position_tolerance: position_tolerance,
         orientation_tolerance: orientation_tolerance, timeout: timeout
 end
-Then(/it is (.*) within (.*)/) do |event_name, timeout|
+Then(/^it is (.*) within (.*)$/) do |event_name, timeout|
     timeout, _ = Roby::App::CucumberHelpers.parse_numerical_value(timeout)
     roby_controller.run_job 'cucumber_job_emits_event',
         monitored_job_id: roby_controller.last_main_job_id, event_name: event_name.to_sym, timeout: timeout
 end
-Then(/the pose is maintained at (.*) with a tolerance of (.*)/) do |pose, tolerance|
+Then(/^the pose is maintained at (.*) with a tolerance of (.*)$/) do |pose, tolerance|
     description = "The pose is maintained at #{pose} with a tolerance of #{tolerance}"
-    pose, position_tolerance, orientation_tolerance = Cucumber::RockHelpers.parse_pose_and_tolerance(pose, tolerance)
+    pose, position_tolerance, orientation_tolerance =
+        Cucumber::RockHelpers.parse_pose_and_tolerance(pose, tolerance)
     roby_controller.start_monitoring_job description, 'cucumber_maintain_pose',
         pose: pose, duration: nil,
         position_tolerance: position_tolerance,
         orientation_tolerance: orientation_tolerance
 end
-Then(/the pose is maintained at (.*) with a tolerance of (.*) during (.*)/) do |pose, tolerance, duration|
+Then(/^the pose is maintained at (.*) with a tolerance of (.*) during (.*)$/) do |pose, tolerance, duration|
     pose, position_tolerance, orientation_tolerance = Cucumber::RockHelpers.parse_pose_and_tolerance(pose, tolerance)
     duration, _ = Roby::App::CucumberHelpers.parse_numerical_value(duration)
     roby_controller.run_job 'cucumber_maintain_pose',
